bf59b4b4be421220e0cd8692d91c833897ec1b52
mWarned <107244005+mWarned@users.noreply.github.com>
2023-12-31 14:52:23 +0200

    Initial commit of the python version

---
+++ Python_Code/main.py
@@ -0,0 +1,17 @@
+import pyperclip
+
+
+def main():
+    return 0
+
+
+def encrypt_message(key, message):
+    return 0
+
+
+def decrypt_message(key, message):
+    return 0
+
+
+def crack_message(message):
+    return 0
